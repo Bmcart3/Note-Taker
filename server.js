0adf14609a9e6ec519c9a7e3c4f16b2c8fb26440
bmcart3 <bmcart3@gmail.com>
2020-06-02 16:56:34 -0400

    get started button now works and directs to notes.html

---
+++ server.js
@@ -0,0 +1,43 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3500;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Notes array
+//==============================================================
+const notes = [];
+
+//Routes
+//==============================================================
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+});
+
+
+
+//Starts the server to begin listening
+//===============================================================
+app.listen(PORT, function() {
+    console.log("App listening on Port " + PORT);
+});
