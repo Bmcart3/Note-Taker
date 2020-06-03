602b5a7b0be4e50a28fc7f19ce32a1ba733d507f
bmcart3 <bmcart3@gmail.com>
2020-06-03 17:01:40 -0400

    switched to .splice instead of filter for delete. still not working properly

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Note-Taker
+
+This app allows the user to create and save notes in a very simple and easy to use format. Node.js and Express were used to create a local server in order to save the information. This was a very challenging project and was my first foray into anything that relates to a backend portion of a website. I will continue to improve upon the functions of this app as I learn more. 
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public", "/notes.html"));
 });
 
-//This should be returning the notes from the db.json I believe. Ask about this.
+//This should be returning the notes from the db.json
 app.get("/api/notes", function (req, res) {
     return res.json(notes);
 });
@@ -39,8 +39,8 @@ app.get("/api/notes", function (req, res) {
 //To create new notes and also needs to save them to db.json.
 app.post("/api/notes", function (req, res) {
     var newNote = req.body;
-    newNote.id = Math.floor(Math.random() * 1000000);
-    console.log(newNote);
+    //Need to find a way to give each note an id equal to 0 then 1 then 2 etc so it will work with my index in the delete functionality i think.
+    newNote.id = Math.floor(Math.random() * 10000);
     notes.push(newNote);
     res.json(newNote);
     
@@ -52,15 +52,12 @@ app.post("/api/notes", function (req, res) {
     });
 });
 
-app.delete("/api/notes/:id", function(req, res) {
-    console.log(req.params.id);
-    let filtered = notes.filter(function(note) {
-        console.log(note);
-        return note.id === req.params.id;
-        
-    });
-    console.log(filtered);
-    fs.writeFile("./db/db.json", JSON.stringify(filtered), err => {
+app.delete("/api/notes/:id", function (req, res) {
+    const index = notes.indexOf(notes.id);
+    const removed = notes.splice(index, 1);
+    res.json(removed[0]);
+
+    fs.writeFile("./db/db.json", JSON.stringify(removed), err => {
         if (err) {
             throw err;
         }
@@ -73,3 +70,27 @@ app.delete("/api/notes/:id", function(req, res) {
 app.listen(PORT, function () {
     console.log("App listening on Port " + PORT);
 });
+
+//Leaving this here for when i return to fix the delete functionality. I may go back to trying filter.
+//===================================================================
+// app.delete("/api/notes/:id", function(req, res) {
+//     const index = parseInt(req.params.index);
+//     const removed = data.splice(index, 1);
+//     res.json(removed[0]);
+// })
+
+
+// app.delete("/api/notes/:id", function(req, res) {
+//     //console.log(req.params.id); //req.params.id is equal to the unique id given in line 42.
+//     const id = parseInt(req.params.id);
+//     let filtered = notes.filter(function(note) {
+//         console.log(note.id); 
+//         return note.id !== id;
+//     });
+    
+//     fs.writeFile("./db/db.json", JSON.stringify(filtered), err => {
+//         if (err) {
+//             throw err;
+//         }
+//     });
+// });
